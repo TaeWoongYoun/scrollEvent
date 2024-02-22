f639b0b35cbb0562395f96f6eac2ed38e1967479
xodnd0507 <24457545yong@gmail.com>
2024-02-22 18:02:00 +0900

    test : Function false

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 //     //윈도우 창 강제로 스크롤시키기(x좌표,y좌표) 
 // })
 
-$(window).on('scroll', function(){
-    $(window).scrollTop()
-})
+// $(window).on('scroll', function(){
+//     $(window).scrollTop()
+// })
+
+
+// div의 스크롤바 내린 양 + 100== div의 실제높이
+// $('lorem').om('scroll', function(){
+//     document.querySelector('.lorem').scrollTop;
+// })
+
+
+
+function window(){
+    console.log('스크롤 감지');
+}
+
+window.addEventListener('scroll', window);
