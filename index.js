42beeb5b4980d57473b86add9aca6a25f607b297
xodnd0507 <24457545yong@gmail.com>
2024-02-22 16:59:43 +0900

    test : Test scroll event

---
+++ index.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function(){
+//     window.scrollTo(0,500);
+//     //윈도우 창 강제로 스크롤시키기(x좌표,y좌표) 
+// })
+
+$(window).on('scroll', function(){
+    $(window).scrollTop()
+})
